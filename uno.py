3a11b8b880e66b1ee70933ce2282d3dcc3d2b697
JulianR <julianrom.co@gmail.com>
2019-10-15 19:52:22 -0500

    Primeras instrucciones de Python

---
+++ uno.py
@@ -0,0 +1,29 @@
+# Este es el comando para imprimir en consola
+print ("Hola soy yo")
+
+# Este es un comentario de una linea
+""" Este es comentario multilinea
+    Este es comentario multilinea
+    Este es comentario multilinea
+"""
+print(100)
+
+print("true")
+
+  # str("test02")
+Lista = ["Borrador", 1, [1 ,2, 3], "Test"]
+
+print (Lista[2])
+
+def test1():
+    return"test"
+
+print (test1())
+
+tupla1 = ("Python", "Django")
+print (tupla1[1])
+
+
+
+
+
